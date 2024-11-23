c52dbc1e3a1d709bc3f7b10cfd990da1d7a49743
Ivo Tops <ivotops@gmail.com>
2024-11-23 23:30:33 +0100

    NET9

--- NoStringEvaluating/Functions/Math/RoundFunction.cs
+++ NoStringEvaluating/Functions/Math/RoundFunction.cs
@@ -29,3 +29,28 @@ public sealed class RoundFunction : IFunction
         return System.Math.Round(args[0].Number, (int)args[1].Number);
     }
 }
+
+/// <summary>
+/// Rounds the designated number to the specified decimals
+/// <para>Round(15.7865; 2)</para>
+/// </summary>
+public sealed class RoundToZeroFunction : IFunction
+{
+    /// <summary>
+    /// Name
+    /// </summary>
+    public string Name { get; } = string.Intern("ROUNDTOZERO");
+
+    /// <summary>
+    /// Can handle IsNull arguments?
+    /// </summary>
+    public bool CanHandleNullArguments { get; }
+
+    /// <summary>
+    /// Execute value
+    /// </summary>
+    public InternalEvaluatorValue Execute(List<InternalEvaluatorValue> args, ValueFactory factory)
+    {
+        return args[0].Number >= 0 ? (long)args[0].Number : -((long)(-args[0].Number));
+    }
+}
